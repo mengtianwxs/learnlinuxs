4ce97f24f7d73ccced0c22ac20eeb6ed0c9983bf
mengtianwxs <374835063@qq.com>
2019-10-05 16:22:24 +0800

    v

--- learnshells/lesson242-shell的扩展/lesson242.sh
+++ learnshells/lesson242-shell的扩展/lesson242.sh
@@ -9,7 +9,19 @@
 #8单词拆分
 #9文件名扩展
 
-#1
+#1 大括号
 echo a{b,c,d,e}f
 #out put:
 #abf acf adf aef
+#整个扩展从左至右
+
+echo {a..z}
+echo {0..10}
+#大括号配合创建三个目录
+#mkdir ~/dir{1..2} 创建dir1 dir2目录
+
+#在大括号扩展只指定一个增加量
+#{<start>..<end>..<incr>}
+
+echo {1..10..3}
+#output 1 4 7 10
